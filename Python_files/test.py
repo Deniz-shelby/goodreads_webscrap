5a6fd4b8fb8c40617f9deced2b4e5ef735d62e8f
Deniz-shelby <d.elci@hotmail.de>
2021-04-19 22:33:23 +0200

    some changes

--- Python_files/test.py
+++ Python_files/test.py
@@ -1,5 +1,10 @@
-from preprocessing import preprocessing
-import pandas as pd
-df = pd.read_csv('../df_1100.csv')
-print(df.head())
-df_cleaned = preprocessing(df)
+import links_extraction as le
+import scrap_and_save as s_s
+import timeit
+
+def main():
+    list_of_1100_links = le.get_all_1000_links('https://www.goodreads.com/list/show/6.Best_Books_of_the_20th_Century?page=')
+    df_all =s_s.create_df_and_save_as_csv(list_of_1100_links)
+
+if __name__=='__main__':
+    main()
